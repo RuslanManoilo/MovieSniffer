f47222d74c9b3bffb5e6cfd856a2766f9b28cbd8
Ruslan <manoiloruslan@gmail.com>
2023-11-25 01:12:24 +0200

    renamed repository name

--- src/index.js
+++ src/index.js
@@ -7,7 +7,7 @@ import { GlobalStyle } from 'components/GlobalStyle';
 
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
-    <BrowserRouter basename="/goit-react-hw-05-movies">
+    <BrowserRouter basename="/MovieSniffer">
       <App />
       <GlobalStyle />
     </BrowserRouter>
